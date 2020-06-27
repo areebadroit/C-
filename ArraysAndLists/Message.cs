e6c612d4d1e90e65f045968a5e25030fb350b098
areebadroit <mdareebansar@gmail.com>
2020-06-28 03:09:28 +0530

    Arrays and Lists

---
+++ ArraysAndLists/Message.cs
@@ -0,0 +1,43 @@
+﻿///Write a  program and continuously ask the user to enter different names, until the user presses Enter(without supplying a name). 
+/// Depending on the number of names provided, display a message based on the above pattern.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    class Message
+    {
+        public void message()
+        {
+            var names = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Type a name (or hit ENTER to quit): ");
+
+                var input = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+                names.Add(input);
+            }
+
+            if (names.Count > 2)
+                Console.WriteLine("{0}, {1} and {2} others like your post", names[0], names[1], names.Count - 2);
+            else if (names.Count == 2)
+                Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
+            else if (names.Count == 1)
+                Console.WriteLine("{0} likes your post.", names[0]);
+            else
+                Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            var msg = new Message();
+            msg.message();
+        }
+    }
+}
---
+++ ArraysAndLists/ReverseString.cs
@@ -0,0 +1,31 @@
+﻿/// Ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. 
+/// Display the reversed name on the console.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    class ReverseString
+    {
+        static void reverse()
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            var reversed = new string(array);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+        static void Main(string[] args)
+        {
+            reverse();
+        }
+    }
+}
