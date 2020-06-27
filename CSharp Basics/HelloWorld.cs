a9a27d18519c98629125dc8325e5295d14947c55
areebadroit <mdareebansar@gmail.com>
2020-06-28 02:15:17 +0530

    init Commit

---
+++ CSharp Basics/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Basics
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# C-
+# C-Sharp
+C# (C-Sharp) is a programming language developed by Microsoft that runs on the . NET Framework. 
+C# is used to develop web apps, desktop apps, mobile apps, games and much more.
+The repository includes some basic codes which I practice in my journey with C# coding, asp.net and MVC.
